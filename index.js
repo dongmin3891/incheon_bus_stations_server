cc4db1f30367d5ccfecdae7dbd18a6de63d21efb
dongmin3891 <rlaehdals753@naver.com>
2021-11-08 13:24:10 +0900

    버스노선조회 api 추가

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.listen(port, function() {
   console.log(`Server app listening at http://localhost:${port}`);
 })
 
+
 const busApiUrl = "http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList"
+const bustRouteApiUrl = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteStationList"
 const serviceKey = "L%2F1NA9bpn1I%2FXr0C2YPtJGNnZXPyrz3O7o3Hvn8ALqFO9DA40GocvMWlaoq6ofkUhaUTcoGKXmTLhkmTz3bVuA%3D%3D"
-//const numOfRows = "2&"
-//const pageNo = "1&"
 const routeId = "232000089"
 
 app.get("/api/test", (req, res) => {
@@ -31,6 +31,7 @@ app.get("/api/test", (req, res) => {
   res.send(JSON.stringify(data));
 })
 
+//버스위치정보
 app.get("/api/busData", (req, res) => {
   console.log("======[GET] busData ======");
   res.setHeader("Content-Type", "application/json");
@@ -49,3 +50,23 @@ app.get("/api/busData", (req, res) => {
     }
   )
 })
+
+//버스노선
+app.get("/api/busRoute", (req, res) => {
+  console.log("======[GET] busRoute =======");
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
+
+  request.get(
+    {
+      uri : `${bustRouteApiUrl}?serviceKey=${serviceKey}&routeId=${routeId}`,
+    },
+  
+    function (error, response, body) {
+      console.log("error", error)
+      res.send(converter.xml2json(body));
+    }
+  )
+});
+
+
