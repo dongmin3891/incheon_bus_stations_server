b3097bde999d9a3e1bc57893def142398872c89b
dongmin3891 <rlaehdals753@naver.com>
2021-11-12 15:03:20 +0900

    포트변경

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const converter = require('xml-js')
 //Express 객체를 생성한다.
 const app = express();
-const port = 3002;
+const port = 443;
 const request = require('request');
 app.listen(port, function() {
   console.log(`Server app listening at http://localhost:${port}`);
