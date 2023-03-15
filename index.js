029dab0d48cfa6530e1dda811f5311ae7dbb8895
dongmin3891 <rlaehdals753@naver.com>
2023-03-15 19:55:32 +0900

    fix: zoom api 서버 셋팅

--- index.js
+++ index.js
@@ -1,126 +1,57 @@
-//Express 모듈을 불러온다.
 const express = require('express');
 const converter = require('xml-js')
-//Express 객체를 생성한다.
 const app = express();
-const https = require('https');
-const port = (process.env.PORT || 443)
+const port = (process.env.PORT || 4000)
+const cors = require('cors');
 const request = require('request');
+const KJUR = require('jsrsasign')
+
 app.listen(port, function() {
   console.log(`Server app listening at http://localhost:${port}`);
 })
+app.use(cors());
 
-//https.createServer(options, app).listen(PORT);
-
-const busApiUrl = "http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList" //버스위치정보
-const bustRouteApiUrl = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteStationList" //버스노선정보
-const busArrivalApiUrl = "http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalItem"
-const serviceKey = "L%2F1NA9bpn1I%2FXr0C2YPtJGNnZXPyrz3O7o3Hvn8ALqFO9DA40GocvMWlaoq6ofkUhaUTcoGKXmTLhkmTz3bVuA%3D%3D"
-const routeId = "232000089" // 김포 90-1번
-const stationId = "168000413" //마전역
-
+const JWT_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlVCYkNTbFJ1UnlHeUx2VGh5VlFtcVEiLCJleHAiOjE2NzkzODEwMzcsImlhdCI6MTY3ODc3NjIzOH0.lwbHgvSCkZSE7LrqWaguiY37lp_ZwhktiKJs59ZdIxs'
 // http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalItem?serviceKey=인증키(URL Encode)&stationId=200000177&routeId=200000037&staOrder=19
 
 app.set('port', port);
 
-app.get("/api/test", (req, res) => {
-  console.log("======[GET] test ======");
+app.get("/api/test", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
+  // res.headers["authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlVCYkNTbFJ1UnlHeUx2VGh5VlFtcVEiLCJleHAiOjE2Nzg4NTY4MjAsImlhdCI6MTY3ODc3MDQyMX0.ZMhUBtNCeb_lm_eYzOTXznz9TKJw86NDppwQka9JL8'
+  
   res.statusCode = 200;
-  let data = {
-    rest_ce: "100",
-    resp_data: {
-      "name" : "kim dong min",
-      "age" : 30
+  // api 요청 할 때마다 토큰을 재생성 해줌으로 사이트가서 안긁어와도 됨
+  // const authorization = generateVideoSdkApiJwt(VIDEO_SDK_API_KEY, VIDEO_SDK_SECRET_KEY)
+
+  const options = {
+    method: 'GET',
+    url: 'https://api.zoom.us/v2/videosdk/sessions?type=past&from=2023-03-10',
+    // url: 'https://api.zoom.us/v2/users',
+
+    headers: {
+      authorization: `Bearer ${JWT_TOKEN}`, // Do not publish or share your token publicly.
     },
-    resp_msg: "정상적으로 처리되었습니다."
   };
-  console.log(data);
-  res.send(JSON.stringify(data));
-})
+  request(options, function(error, response, body) {
+      res.send(body);
+  })
+});
 
-// //버스위치정보
-// app.get("/api/busData", (req, res) => {
-//   console.log("======[GET] busData ======");
-//   res.setHeader("Content-Type", "application/json");
-//   // res.setHeader("Access-Control-Allow-Origin", "*");
-//   // res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.statusCode = 200;
-  
-//   request.get(
-//     {
-//       uri: `${busApiUrl}?serviceKey=${serviceKey}&routeId=${routeId}`,
-//     },
-//     function (error, response, body) {
-//       console.log("body", body);
-//       console.log("error", error);
-//       res.send(converter.xml2json(body));
-//     }
-//   )
-// })
+function generateVideoSdkApiJwt(sdkApiKey, sdkApiSecret) {
 
-// //버스노선
-// app.get("/api/busRoute", (req, res) => {
-//   console.log("======[GET] busRoute =======");
-//   res.setHeader("Content-Type", "application/json");
-//   res.statusCode = 200;
+  const iat = Math.round((new Date().getTime() - 30000) / 1000)
+  const exp = iat + 60 * 60 * 2
+  const oHeader = { alg: 'HS256', typ: 'JWT' }
 
-//   request.get(
-//     {
-//       uri : `${bustRouteApiUrl}?serviceKey=${serviceKey}&routeId=${routeId}`,
-//     },
-  
-//     function (error, response, body) {
-//       console.log("error", error)
-//       res.send(converter.xml2json(body));
-//     }
-//   )
-// });
+  const oPayload = {
+    iss: sdkApiKey,
+    iat: iat,
+    exp: exp
+  }
 
-
-// //버스도착정보조회
-// app.get("/api/busArrival", (req, res) => {
-//   console.log("======[GET] busArrival =======");
-//   res.setHeader("Content-Type", "application/json");
-//   res.statusCode = 200;
-
-//   request.get(
-//     {
-//       uri : `${busArrivalApiUrl}?serviceKey=${serviceKey}&stationId=${stationId}&routeId=${routeId}`,
-//     },
-  
-//     function (error, response, body) {
-//       console.log("error", error)
-//       res.send(converter.xml2json(body));
-//     }
-//   )
-// });
-
-// app.get("*", function (req, res) {
-//   var file = path.join(dir, req.path.replace(/\/$/, "/index.html"));
-//   console.log("file: " + file);
-//   if (file.indexOf(dir + path.sep) !== 0) {
-//       return res
-//           .status(403)
-//           .end("Forbidden");
-//   }
-//   var type = mime[
-//       path
-//           .extname(file)
-//           .slice(1)
-//           .toLowerCase()
-//   ] || "text/plain";
-//   console.log("type: " + type);
-
-//   var s = fs.createReadStream(file);
-//   s.on("open", function () {
-//       res.set("Content-Type", type);
-//       s.pipe(res);
-//   });
-//   s.on("error", function () {
-//       res.set("Content-Type", "text/plain");
-//       res
-//           .status(404)
-//           .end("Not found");
-//   });
-// })
+  const sHeader = JSON.stringify(oHeader)
+  const sPayload = JSON.stringify(oPayload)
+  const videosdk_api_token = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, sdkApiSecret)
+  return videosdk_api_token
+}
